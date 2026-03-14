2f0e1bf44c64a2d53ff133cb59367e90de7ff597
Yevhenii Slavin <evgenijslavin@gmail.com>
2026-03-14 17:00:47 +0200

    Reverse 10 bytes

---
+++ raw/reverse_10_bytes.s
@@ -0,0 +1,34 @@
+; Table wiht data 0200 - 02ff
+; The goal is to reverse order of the first 10 bytes and store them starting at $0300
+; (Assuming that the table is zeroed out) 
+
+; Table start address
+lda #$00
+sta $00
+lda #$02
+sta $01
+
+
+; Prepare some data in the table. The sum must be $268
+lda #$55
+sta $0200
+sta $0201
+sta $0202
+lda #$3f
+sta $0205
+sta $0206
+lda #$03
+sta $0207
+sta $0208
+lda #$e5
+sta $0209
+
+ldy #9
+ldx #0
+
+LOOP:
+  lda $0200, y
+  sta $0300, x
+  inx
+  dey
+  bpl LOOP
